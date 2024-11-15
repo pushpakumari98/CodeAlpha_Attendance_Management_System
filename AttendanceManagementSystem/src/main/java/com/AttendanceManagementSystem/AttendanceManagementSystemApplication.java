1b6d2702ca8061bec423b5e1859bcbaff253f66c
pushpakumari98 <pushpa20052002kumari@gmail.com>
2024-11-15 19:30:18 +0530

    update

--- AttendanceManagementSystem/src/main/java/com/AttendanceManagementSystem/AttendanceManagementSystemApplication.java
+++ AttendanceManagementSystem/src/main/java/com/AttendanceManagementSystem/AttendanceManagementSystemApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AttendanceManagementSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AttendanceManagementSystemApplication.class, args);
 	}
